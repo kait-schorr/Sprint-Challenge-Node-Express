b67c200ce35499260c89bef2589596a5fe73e73a
kait-schorr <kaitlynschorr@gmail.com>
2018-04-06 12:30:14 -0500

    Finished post and put and delete in the actionRouter

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -12,4 +12,22 @@ router.get("/:id", (req, res) => {
   db.get(req.params.id).then(action => res.json(action));
 });
 
+router.post("/", (req, res) => {
+  db.insert(req.body).then(action => {
+    res.json(action);
+  });
+});
+
+router.put("/:id", (req, res) => {
+  db.update(req.params.id, req.body).then(action => {
+    res.json(action);
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  db.remove(req.params.id).then(count => {
+    res.json(count);
+  });
+});
+
 module.exports = router;
