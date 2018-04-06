73e8893d42f179d1372c86a0bf766a73c7820516
kait-schorr <kaitlynschorr@gmail.com>
2018-04-06 12:55:29 -0500

    Added catches to all the CRUD methods

--- actions/actionRouter.js
+++ actions/actionRouter.js
@@ -5,29 +5,64 @@ const db = require("./actionModel");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  db.get().then(action => res.json(action));
+  db
+    .get()
+    .then(action => res.json(action))
+    .catch(err => {
+      res.status(500).json({
+        message: "There was a problem getting the actions from the server."
+      });
+    });
 });
 
 router.get("/:id", (req, res) => {
-  db.get(req.params.id).then(action => res.json(action));
+  db
+    .get(req.params.id)
+    .then(action => res.json(action))
+    .catch(err => {
+      res.status(500).json({
+        message: "There was a problem getting the action from the server."
+      });
+    });
 });
 
 router.post("/", (req, res) => {
-  db.insert(req.body).then(action => {
-    res.json(action);
-  });
+  db
+    .insert(req.body)
+    .then(action => {
+      res.json(action);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There was a problem posting the action to the server."
+      });
+    });
 });
 
 router.put("/:id", (req, res) => {
-  db.update(req.params.id, req.body).then(action => {
-    res.json(action);
-  });
+  db
+    .update(req.params.id, req.body)
+    .then(action => {
+      res.json(action);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There was a problem updating the action on the server."
+      });
+    });
 });
 
 router.delete("/:id", (req, res) => {
-  db.remove(req.params.id).then(count => {
-    res.json(count);
-  });
+  db
+    .remove(req.params.id)
+    .then(count => {
+      res.json(count);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There was a problem deleting the action from the server."
+      });
+    });
 });
 
 module.exports = router;
--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -3,39 +3,82 @@ const db = require("./projectModel");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  db.get().then(projects => {
-    res.json(projects);
-  });
+  db
+    .get()
+    .then(projects => {
+      res.json(projects);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There was a problem getting the projects from the server."
+      });
+    });
 });
 
 router.get("/:id", (req, res) => {
-  db.get(req.params.id).then(projects => {
-    res.json(projects);
-  });
+  db
+    .get(req.params.id)
+    .then(projects => {
+      res.json(projects);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There was a problem getting the project from the server."
+      });
+    });
 });
 
 router.get("/:id/actions", (req, res) => {
-  db.getProjectActions(req.params.id).then(actions => {
-    res.json(actions);
-  });
+  db
+    .getProjectActions(req.params.id)
+    .then(actions => {
+      res.json(actions);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message:
+          "There was a problem getting the project's actions from the server."
+      });
+    });
 });
 
 router.post("/", (req, res) => {
-  db.insert(req.body).then(project => {
-    res.json(project);
-  });
+  db
+    .insert(req.body)
+    .then(project => {
+      res.json(project);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There was a problem posting the project to the server."
+      });
+    });
 });
 
 router.put("/:id", (req, res) => {
-  db.update(req.params.id, req.body).then(project => {
-    res.json(project);
-  });
+  db
+    .update(req.params.id, req.body)
+    .then(project => {
+      res.json(project);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There was a problem updating the project on the server."
+      });
+    });
 });
 
 router.delete("/:id", (req, res) => {
-  db.remove(req.params.id).then(count => {
-    res.json(count);
-  });
+  db
+    .remove(req.params.id)
+    .then(count => {
+      res.json(count);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There was a problem deleting the project from the server."
+      });
+    });
 });
 
 module.exports = router;
